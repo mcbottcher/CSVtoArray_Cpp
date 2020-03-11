451463c2f9f20f0034e1b476ace523990ed5823f
mcbottcher <mcbottcher@gmail.com>
2020-03-11 11:50:27 +0000

    read whole csv into single 2D array

--- main.cpp
+++ main.cpp
@@ -14,12 +14,15 @@ int main(){
 	ifstream myfile ("movement1.csv");
 	if (myfile.is_open())
 	{
+	
+		long int li[220][19];
+		int j=0;
 		while ( getline (myfile,line) )
 		{
 		
 		//need to put this string line and parse it into its numbers into a vector...
 		char * pEnd;
-		long int li[220];
+		
 		
 		for(int i = 0; i < line.size(); i++) {
 			if(line[i]==','){
@@ -31,17 +34,14 @@ int main(){
 		const char * start = line.c_str();
 		
 		for(int i=0;i<220;i++){
-			li[i] = strtol (start, &pEnd, 10);
+			li[i][j] = strtol (start, &pEnd, 10);
 			start = pEnd;
 			
 		}
-		
-		//we have each in a long int array
-		//need to convert to vector of vector
-		
-	  	
+		j++;
 		}
 		myfile.close();
+		
 	}
 
 	else cout << "Unable to open file"; 
