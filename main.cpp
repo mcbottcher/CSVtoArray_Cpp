cf4597845cfebaf2fb2bc301f46f93cd6aa8f91b
mcbottcher <mcbottcher@gmail.com>
2020-03-12 13:52:47 +0000

    now in vector format

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <fstream>
+#include <vector>
 
 #include "CSVreader.h"
 
@@ -12,39 +13,57 @@ int main(){
 	
 	string line;
 	ifstream myfile ("movement1.csv");
+	long int li[220];
+	//convert to a vector..
+	vector<vector<int>> myvector;
+	vector<int> temp;
+	
 	if (myfile.is_open())
 	{
 	
-		long int li[220][19];
-		int j=0;
 		while ( getline (myfile,line) )
 		{
 		
-		//need to put this string line and parse it into its numbers into a vector...
-		char * pEnd;
-		
-		
-		for(int i = 0; i < line.size(); i++) {
-			if(line[i]==','){
-				line[i] = ' ';
-			}
+		        //need to put this string line and parse it into its numbers into a vector...
+		        char * pEnd;
+		        
+		        for(int i = 0; i < line.size(); i++) {
+			        if(line[i]==','){
+				        line[i] = ' ';
+			        }
+		        }
+		        
+		        const char * start = line.c_str();
+		        
+		        for(int i=0;i<220;i++){
+			        li[i] = strtol (start, &pEnd, 10);
+			        start = pEnd;
+			        temp.push_back(li[i]);
+			        
+		        }
+		        
+		        myvector.push_back(temp);
+		        temp.clear();
+		     
+	
 		}
 		
 		
-		const char * start = line.c_str();
-		
-		for(int i=0;i<220;i++){
-			li[i][j] = strtol (start, &pEnd, 10);
-			start = pEnd;
-			
-		}
-		j++;
-		}
 		myfile.close();
+		
+		for(int i=0; i<myvector.size();i++){
+		        for(int k=0; k<myvector[i].size();k++){
+		                cout << myvector[i][k] << " " ;
+		        }
+		        
+		        cout << endl << endl;
+		
+		}
 		
 	}
 
 	else cout << "Unable to open file"; 
+	
 
 	return 0;
 	
